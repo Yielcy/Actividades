b980130b431ed28d50ef54b568842c5884a2fc9b
Yielcy <jonthan.galio108@gmail.com>
2021-10-27 14:52:38 -0500

    App

--- ProyectoLogeo/src/main/java/mx/uv/App.java
+++ ProyectoLogeo/src/main/java/mx/uv/App.java
@@ -1,12 +1,27 @@
 package mx.uv;
 
 import static spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.port;
 import com.google.gson.*;
 
 public class App 
 {
     public static void main( String[] args ){
-        System.out.println( "Hello World!" );
-        //get("/hello", {rq, res} -> 'Hello World');
+        staticFiles.location("/");
+        port(getHerokuAssignedPort());
+        get("/", (req, res) -> "respuesta");
+        get("/pagina", (req, res) -> {
+            res.redirect("login.html");
+            return null;
+        });
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
